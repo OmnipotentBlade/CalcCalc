724f8ecfa7008bf3ce2aa0108776025efa07695f
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2022-12-28 15:54:35 -0500

    Added new Parser class

---
+++ Parser.java
@@ -0,0 +1,12 @@
+public class Parser {
+    
+    private static String parserInput;
+    private static String[] parserOutput;
+
+    public static String[] parseString(String input) {
+        String delim = "[ ]+";
+        String[] function = input.split(delim);
+
+        return parserOutput;
+    }
+}
